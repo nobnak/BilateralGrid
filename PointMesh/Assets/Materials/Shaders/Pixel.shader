11fcac1d398e20d8bd4e9bfab25654e1faa6febe
nnakata <nakata@team-lab.com>
2013-12-18 13:38:15 +0900

    poin mesh の実験

---
+++ PointMesh/Assets/Materials/Shaders/Pixel.shader
@@ -0,0 +1,38 @@
+﻿Shader "Custom/Pixel" {
+	Properties {
+		_MainTex ("Base (RGB)", 2D) = "white" {}
+	}
+	SubShader {
+		Tags { "RenderType"="Transparent" "Queue"="Transparent" "IgnoreProjector"="True" }
+		LOD 200
+		
+		Pass {
+			CGPROGRAM
+			#pragma vertex vert
+			#pragma fragment frag
+			#include "UnityCG.cginc"
+	
+			sampler2D _MainTex;
+
+			struct vsout {
+				float4 vertex : POSITION;
+				float2 texcoord : TEXCOORD0;
+				float4 color : TEXCOORD1;
+			};
+
+			vsout vert(appdata_full i) {
+				vsout o;
+				o.vertex = mul(UNITY_MATRIX_MVP, i.vertex);
+				o.texcoord = i.texcoord.xy;
+				o.color = i.color;
+				return o;
+			}
+			
+			fixed4 frag(vsout i) : COLOR {
+				return i.color;
+			}
+			ENDCG
+		}
+	} 
+	FallBack "Diffuse"
+}
---
+++ PointMesh/Assets/Scripts/PixelVertices.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PixelVertices : MonoBehaviour {
+	public Texture2D src;
+
+	private Mesh _mesh;
+
+	// Use this for initialization
+	void Start () {
+		var width = src.width;
+		var height = src.height;
+
+		var vertices = new Vector3[width * height];
+		var indices = new int[width * height];
+		var colors = src.GetPixels();
+
+		for (var y = 0; y < height; y++) {
+			for (var x = 0; x < width; x++) {
+				var index = x + y * width;
+				vertices[index] = new Vector3(x, y, 0);
+				indices[index] = index;
+			}
+		}
+
+		_mesh = new Mesh();
+		_mesh.vertices = vertices;
+		_mesh.colors = colors;
+		_mesh.SetIndices(indices, MeshTopology.Points, 0);
+		_mesh.bounds = new Bounds(new Vector3(width * 0.5f, height * 0.5f, 0f), new Vector3(width, height, 0));
+
+		var mf = GetComponent<MeshFilter>();
+		mf.sharedMesh = _mesh;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnDestroy() {
+		Destroy(_mesh);
+	}
+}
