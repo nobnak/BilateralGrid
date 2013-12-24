b9ec164454d57ac954519e471c9031ec83cfd7aa
nobnak <nobu.nakata@gmail.com>
2013-12-25 03:21:31 +0900

    Revert "renamed"
    This reverts commit 35c313c216392558fa72fd729baee1d593b1768c.

---
+++ ParticleSphere/Assets/Materials/Shaders/NormalView.shader
@@ -0,0 +1,37 @@
+﻿Shader "Custom/NormalView" {
+	Properties {
+		_MainTex ("Base (RGB)", 2D) = "white" {}
+	}
+	SubShader {
+		ZTest Always Cull Off ZWrite Off
+		Fog { Mode off }
+		
+		Pass {
+			CGPROGRAM
+			#include "UnityCG.cginc"
+			#pragma vertex vert
+			#pragma fragment frag
+
+			sampler2D _MainTex;
+			
+			struct v2f {
+				float4 vertex : POSITION;
+				float2 texcoord : TEXCOORD0;
+			};
+			
+			v2f vert(appdata_base i) {
+				v2f o;
+				o.vertex = mul(UNITY_MATRIX_MVP, i.vertex);
+				o.texcoord = i.texcoord.xy;
+				return o;
+			}
+			
+			fixed4 frag(v2f i) : COLOR {
+				float3 n = tex2D(_MainTex, i.texcoord).rgb;
+				return float4(n * 0.5 + 0.5, 1.0);
+			}
+			ENDCG
+		}
+	} 
+	FallBack "Diffuse"
+}
---
+++ ParticleSphere/Assets/Scripts/Editor/SpriteMeshGen.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+
+public class SpriteMeshGen {
+	[MenuItem("Assets/Custom/SpriteMesh")]
+	public static void GenSpriteMesh() {
+		var mesh = new Mesh();
+		var vertices = new Vector3[]{ Vector3.zero, Vector3.zero, Vector3.zero, Vector3.zero };
+		var triangles = new int[]{ 0, 3, 1, 0, 2, 3 };
+		var uv = new Vector2[]{ new Vector2(0f, 0f), new Vector2(1f, 0f), new Vector2(0f, 1f), new Vector2(1f, 1f) };
+		var normals = new Vector3[]{ Vector3.back, Vector3.back, Vector3.back, Vector3.back };
+		
+		mesh.vertices = vertices;
+		mesh.triangles = triangles;
+		mesh.uv = uv;
+		mesh.normals = normals;
+		mesh.bounds = new Bounds(Vector3.zero, new Vector3(2f, 2f, 0f));
+
+		AssetDatabase.CreateAsset(mesh, "Assets/Generated/Sprite.asset");
+	}
+}
